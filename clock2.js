fd5bb6f797ac8f82b30bd335c3024d134c2fc9bc
Flub78 <frederic.peignot@free.fr>
2022-06-22 17:39:20 +0200

    Cosmetic changes

--- clock2.js
+++ clock2.js
@@ -7,16 +7,20 @@ const sc = document.querySelector('#sc');
 
 /*
  * Every second, get the local time 
- * and rotate the needles accordingly
+ * and rotate the hands accordingly
  */
 setInterval(() => {
     let day = new Date();
+
+    // dd, mm, ss are the bearing in ° of the hands
+    // 0 for 12 oclock; 90 for 3 oclock, etc.
     let hh = day.getHours() * 30;
     let mm = day.getMinutes() * deg;
     let ss = (day.getSeconds() + (day.getMilliseconds() / 1000)) * deg;
 
+    // console.log('date', hh, mm, ss)
     // Milliseconds are only useful to refresh more than once per second to
-    // get a smoothlier move of the second needle.
+    // get a smoothlier move of the second hand.
 
     hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
     mn.style.transform = `rotateZ(${mm}deg)`;

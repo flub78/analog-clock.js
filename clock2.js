3d6651bf42c851da1c7d29dd0cc36f9a57c930b5
Flub78 <frederic.peignot@free.fr>
2022-06-22 14:59:36 +0200

    Analysis in progress, still some questions on how the needles are drawn

--- clock2.js
+++ clock2.js
@@ -1,3 +1,24 @@
 // Javascript for an analog clock
 
-// alert('clock 2');
+const deg = 6;
+const hr = document.querySelector('#hr');
+const mn = document.querySelector('#mn');
+const sc = document.querySelector('#sc');
+
+/*
+ * Every second, get the local time 
+ * and rotate the needles accordingly
+ */
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = (day.getSeconds() + (day.getMilliseconds() / 1000)) * deg;
+
+    // Milliseconds are only useful to refresh more than once per second to
+    // get a smoothlier move of the second needle.
+
+    hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+}, 1000)
